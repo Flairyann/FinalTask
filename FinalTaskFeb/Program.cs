f47af9b8d903e0da54b4f63b984b1c822af0fbe0
Annie <flairyann@gmail.com>
2022-02-28 19:57:53 +0300

    Count for new array

--- FinalTaskFeb/Program.cs
+++ FinalTaskFeb/Program.cs
@@ -2,3 +2,15 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+string[] sourceArray = {"hello", "12345", "red", "world", "1984", "cat", "computer science", ":-)"};
+
+int CountNewArray(string[] array)
+{
+    int newCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i].Length <=3)
+          newCount++; 
+    }
+    return newCount;
+}
